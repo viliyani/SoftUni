160c55c3e4970cdc238ddc4750f53a1056679813
Viliyan <viliyan7@gmail.com>
2020-10-20 16:56:14 +0300

    C# Advanced - Lab 07 - Generics

---
+++ C# Advanced/C# Advanced - Labs/Lab-07-Generics/Task01/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public T Remove()
+        {
+            T item = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return item;
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - Labs/Lab-07-Generics/Task03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> check = new EqualityScale<int>(1,1);
+
+            Console.WriteLine(check.AreEqual());
+        }
+    }
+}
