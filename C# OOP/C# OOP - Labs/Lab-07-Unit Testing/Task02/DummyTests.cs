6bba387a453077f6b586c6ec8b62bd74fc51f687
Viliyani <viliyan7@gmail.com>
2020-12-14 17:13:23 +0200

    C# OOP - Lab 07 - Unit Testing

---
+++ C# OOP/C# OOP - Labs/Lab-07-Unit Testing/Task02/DummyTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace Skeleton
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLoosesHealthAfterAttack()
+        {
+            Dummy dummy = new Dummy(100, 50);
+
+            dummy.TakeAttack(20);
+
+            Assert.That(dummy.Health, Is.EqualTo(80), "Dummy Health is not loosing after take attack.");
+        }
+
+        [Test]
+        public void DeadDummyThrowsExceptionIfAttacked()
+        {
+            Dummy dummy = new Dummy(0, 50);
+
+            Assert.That(() => dummy.TakeAttack(20),
+              Throws.InvalidOperationException
+                .With.Message.EqualTo("Dummy is dead."));
+        }
+
+        [Test]
+        public void DeadDummyShouldGiveXP()
+        {
+            Dummy dummy = new Dummy(0, 50);
+
+            var experience = dummy.GiveExperience();
+
+            Assert.That(experience, Is.EqualTo(50), "Dead dummies give experience.");
+        }
+
+        [Test]
+        public void AliveDummyShouldntGiveXP()
+        {
+            Dummy dummy = new Dummy(100, 50);
+
+            Assert.That(() =>
+                dummy.GiveExperience(),
+                Throws.InvalidOperationException.
+                With.Message.EqualTo("Target is not dead."));
+        }
+    }
+}
---
+++ C# OOP/C# OOP - Labs/Lab-07-Unit Testing/Task03/AxeTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace Skeleton
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        private const string AXE_DURABILITY_RESULT_FAILED_MSG = "Axe durability doesn't change after attack.";
+
+        private const string AXE_BROKEN_MSG = "Axe is broken.";
+
+        private const int ATTACKS_POINTS = 10;
+
+        private Dummy dummy;
+
+        [SetUp]
+        public void SetDummy()
+        {
+            this.dummy = new Dummy(ATTACKS_POINTS, 10);
+        }
+
+        [Test]
+        public void AxeLoosesDurabilityAfterAttack()
+        {
+            // Arrange
+            Axe axe = new Axe(ATTACKS_POINTS, 10);
+
+            // Act
+            axe.Attack(this.dummy);
+
+            // Assert
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(9), AXE_DURABILITY_RESULT_FAILED_MSG);
+        }
+
+        [Test]
+        public void BrokenAxeCantAttack()
+        {
+            Axe axe = new Axe(ATTACKS_POINTS, 0);
+
+            Assert.That(() => axe.Attack(dummy),
+              Throws.InvalidOperationException
+                .With.Message.EqualTo(AXE_BROKEN_MSG));
+        }
+    }
+}
