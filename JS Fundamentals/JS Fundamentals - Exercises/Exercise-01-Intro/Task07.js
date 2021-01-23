a238555fe14f89c799ad1bb0a98823ee7b1b379b
Viliyani <viliyan7@gmail.com>
2021-01-23 19:57:52 +0200

    Deleted Exercise 01

--- JS Fundamentals/JS Fundamentals - Exercises/Exercise-01-Intro/Task07.js
+++
@@ -1,7 +0,0 @@
-function solve(num) {
-    for (let i = 1; i <= num; i++) {
-        let result = "";
-        result += i + " ";
-        console.log(result.repeat(i));
-    }
-}
--- JS Fundamentals/JS Fundamentals - Exercises/Exercise-01-Intro/Task08.js
+++
@@ -1,7 +0,0 @@
-function solve(num) {
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${num} X ${i} = ${num * i}`);
-    }
-}
-
-solve(5);
