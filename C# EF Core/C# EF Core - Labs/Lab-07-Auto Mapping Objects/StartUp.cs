c8b5851e262a24e606eca2b975653e1503b0e08f
Viliyan <viliyan7@gmail.com>
2021-03-22 18:53:54 +0200

    C# EF Core - Lab 07 - Auto Mapping Objects

---
+++ C# EF Core/C# EF Core - Labs/Lab-07-Auto Mapping Objects/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using LinqDemo.Data;
+using AutoMapper;
+using LinqDemo.Models;
+
+namespace LinqDemo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var db = new MusicXContext();
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Song, SongInfoDto>();
+            });
+
+            var mapper = config.CreateMapper();
+
+            Song song = db.Songs.Where(x => x.Id == 10).First();
+
+            var songDto = mapper.Map<SongInfoDto>(song);
+        }
+    }
+
+    public class SongInfoDto
+    {
+        public string Name { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string SourceName { get; set; }
+    }
+}
