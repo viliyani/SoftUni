0c179e9e635036321d3a659613134e9af9095640
Viliyan <viliyan7@gmail.com>
2021-03-28 20:56:30 +0300

    C# EF Core - Lab 09 - Xml Processing

---
+++ C# EF Core/C# EF Core - Labs/Lab-09-Xml Processing/Demo-Simple Xml/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XmlDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            XDocument document = XDocument.Load("Data.xml");
+
+            int booksCount = document.Root.Elements().Count();
+            Console.WriteLine($"Total books: {booksCount}");
+
+            foreach (var element in document.Root.Elements())
+            {
+                Console.WriteLine(element.Element("title").Value);
+            }
+
+        }
+    }
+}
