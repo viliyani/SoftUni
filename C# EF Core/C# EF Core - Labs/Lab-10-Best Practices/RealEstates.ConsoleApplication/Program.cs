e040f4382397cc2e54dd1cfafc72057a0cefae49
Viliyan <viliyan7@gmail.com>
2021-03-31 22:19:09 +0300

    C# EF Core - Lab 10 - Best Practices - part 1

---
+++ C# EF Core/C# EF Core - Labs/Lab-10-Best Practices/RealEstates.ConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstates.Data;
+using RealEstates.Models;
+using System;
+
+namespace RealEstates.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new ApplicationDbContext();
+            db.Database.Migrate();
+
+            db.Districts.Add(new District { Name = "Дианабад" });
+            db.SaveChanges();
+        }
+    }
+}
---
+++ C# EF Core/C# EF Core - Labs/Lab-10-Best Practices/RealEstates.Services/IPropertiesService.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstates.Services
+{
+    public interface IPropertiesService
+    {
+        void Add(string district, int floor, int maxFloors, int size, int yardSize, int year, string propertyType, string buildingType, int price);
+
+    }
+}
