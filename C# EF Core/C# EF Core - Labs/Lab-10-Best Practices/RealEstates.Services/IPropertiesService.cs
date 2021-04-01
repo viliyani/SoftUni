139111435e56da6502c66c67d0b60e0c1c1204bb
Viliyan <viliyan7@gmail.com>
2021-04-01 18:20:13 +0300

    C# EF Core - Lab 10 - Best Practices - part 2

--- C# EF Core/C# EF Core - Labs/Lab-10-Best Practices/RealEstates.Services/IPropertiesService.cs
+++ C# EF Core/C# EF Core - Labs/Lab-10-Best Practices/RealEstates.Services/IPropertiesService.cs
@@ -1,8 +1,16 @@
-﻿namespace RealEstates.Services
+﻿using RealEstates.Services.Models;
+using System.Collections.Generic;
+
+namespace RealEstates.Services
 {
     public interface IPropertiesService
     {
-        void Add(string district, int floor, int maxFloors, int size, int yardSize, int year, string propertyType, string buildingType, int price);
+        void Add(string district, int price,
+            int floor, int maxFloors, int size, int yardSize, 
+            int year, string propertyType, string buildingType);
+
+        IEnumerable<PropertyInfoDto> Search(int minPrice, int maxPrice, int minSize, int maxSize);
+
 
     }
 }
---
+++ C# EF Core/C# EF Core - Labs/Lab-10-Best Practices/RealEstates.Services/PropertiesService.cs
@@ -0,0 +1,79 @@
+﻿using RealEstates.Data;
+using RealEstates.Models;
+using RealEstates.Services.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RealEstates.Services
+{
+    public class PropertiesService : IPropertiesService
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public PropertiesService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Add(string district, int price,
+            int floor, int maxFloors, int size, int yardSize,
+            int year, string propertyType, string buildingType)
+        {
+            var property = new Property
+            {
+                Size = size,
+                Price = price <= 0 ? null : price,
+                Floor = floor <= 0 || floor > 255 ? null : (byte)floor,
+                TotalFloors = maxFloors <= 0 || floor > 255 ? null : (byte)maxFloors,
+                YardSize = yardSize <= 0 ? null : yardSize,
+                Year = year <= 0 ? null : year,
+            };
+
+            var dbDistrict = dbContext.Districts.FirstOrDefault(x => x.Name == district);
+            if (dbDistrict == null)
+            {
+                dbDistrict = new District { Name = district };
+            }
+
+            property.District = dbDistrict;
+
+            var dbPropertyType = dbContext.PropertyTypes.FirstOrDefault(x => x.Name == propertyType);
+            if (dbPropertyType == null)
+            {
+                dbPropertyType = new PropertyType { Name = propertyType };
+            }
+
+            property.Type = dbPropertyType;
+
+            var dbBuildingType = dbContext.Buildings.FirstOrDefault(x => x.Name == buildingType);
+            if (dbBuildingType == null)
+            {
+                dbBuildingType = new BuildingType { Name = buildingType };
+            }
+
+            property.BuildingType = dbBuildingType;
+
+            dbContext.Properties.Add(property);
+            dbContext.SaveChanges();
+
+        }
+
+        public IEnumerable<PropertyInfoDto> Search(int minPrice, int maxPrice, int minSize, int maxSize)
+        {
+            var properties = dbContext
+                .Properties
+                .Where(x => x.Price >= minPrice && x.Price <= maxPrice && x.Size >= minSize && x.Size <= maxSize)
+                .Select(x => new PropertyInfoDto
+                {
+                    Size = x.Size,
+                    Price = x.Price ?? 0,
+                    BuildingType = x.BuildingType.Name,
+                    DistrictName = x.District.Name,
+                    PropertyType = x.Type.Name
+                })
+                .ToList();
+
+            return properties;
+        }
+    }
+}
