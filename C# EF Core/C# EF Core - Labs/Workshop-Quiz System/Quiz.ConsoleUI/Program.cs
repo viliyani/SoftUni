13633686f43400b51835330ffd262a992bceb77b
Viliyan <viliyan7@gmail.com>
2021-04-09 23:50:46 +0300

    C# EF Core - Workshop - Quiz System - part 1

---
+++ C# EF Core/C# EF Core - Labs/Workshop-Quiz System/Quiz.ConsoleUI/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Quiz.Data;
+using Quiz.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Quiz.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // Import data from JSON
+            var json = File.ReadAllText("EF-Core-Quiz.json");
+            var questions = JsonConvert.DeserializeObject<IEnumerable<JsonQuestion>>(json);
+
+            var quizService = serviceProvider.GetService<IQuizService>();
+            var questionService = serviceProvider.GetService<IQuestionService>();
+            var answerService = serviceProvider.GetService<IAnswerService>();
+
+            var quizId = quizService.Add("EF Core Test");
+
+            foreach (var question in questions)
+            {
+                var questionId = questionService.Add(question.Question, quizId);
+                foreach (var answer in question.Answers)
+                {
+                    answerService.Add(answer.Answer, answer.Correct ? 1 : 0, answer.Correct, questionId);
+                }
+            }
+
+
+
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            services.AddDbContext<ApplicationDbContext>(options
+                => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+              .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddTransient<IQuizService, QuizSerivce>();
+            services.AddTransient<IQuestionService, QuestionService>();
+            services.AddTransient<IAnswerService, AnswerService>();
+            services.AddTransient<IUserAnswerService, UserAnswerService>();
+        }
+    }
+}
---
+++ C# EF Core/C# EF Core - Labs/Workshop-Quiz System/Quiz.Data/Migrations/20210409121959_ModelUpdates.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quiz.Data.Migrations
+{
+    public partial class ModelUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAnswers_Quizes_QuizId",
+                table: "UserAnswers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserAnswers_QuizId",
+                table: "UserAnswers");
+
+            migrationBuilder.DropColumn(
+                name: "QuizId",
+                table: "UserAnswers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuizId",
+                table: "UserAnswers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAnswers_QuizId",
+                table: "UserAnswers",
+                column: "QuizId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAnswers_Quizes_QuizId",
+                table: "UserAnswers",
+                column: "QuizId",
+                principalTable: "Quizes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
