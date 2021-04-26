f911ad3a628ce263a21fd1df5dccb8744e46ed79
Viliyan <viliyan7@gmail.com>
2021-04-26 20:11:53 +0300

    C# Web Basics - Lab 05 - MVC Intro

---
+++ C# Web Basics/C# Web Basics - Labs/Lab-05-MVC Intro/SUS.MvcFramework/Host.cs
@@ -0,0 +1,21 @@
+﻿using SUS.HTTP;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SUS.MvcFramework
+{
+    public class Host
+    {
+        public static async Task CreateHostAsync(IMvcApplication application, int port)
+        {
+            List<Route> routeTable = new List<Route>();
+            application.ConfigureServices();
+            application.Configure(routeTable);
+
+            IHttpServer server = new HttpServer(routeTable);
+
+            await server.StartAsync(port);
+        }
+
+    }
+}
