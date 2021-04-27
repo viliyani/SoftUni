d0240155f96aabb9fc705886de742a8918a201fc
Viliyan <viliyan7@gmail.com>
2021-04-27 22:51:58 +0300

    C# Web Basics - Lab 06 - MVC Custom View Engine

---
+++ C# Web Basics/C# Web Basics - Labs/Lab-06-MVC-Custom View Engine/Apps/MyFirstMvcApp/Controllers/StaticFilesController.cs
@@ -0,0 +1,34 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public class StaticFilesController : Controller
+    {
+        public HttpResponse Favicon(HttpRequest request)
+        {
+            return File("wwwroot/favicon.ico", "image/vnd.microsoft.icon");
+        }
+
+        public HttpResponse BootstrapCss(HttpRequest arg)
+        {
+            return File("wwwroot/css/bootstrap.min.css", "text/css");
+        }
+
+        public HttpResponse CustomCss(HttpRequest arg)
+        {
+            return File("wwwroot/css/custom.css", "text/css");
+        }
+
+        public HttpResponse CustomJs(HttpRequest arg)
+        {
+            return File("wwwroot/js/custom.js", "text/javascript");
+        }
+
+        public HttpResponse BootstrapJs(HttpRequest arg)
+        {
+            return File("wwwroot/js/bootstrap.bundle.min.js", "text/javascript");
+        }
+    }
+}
---
+++ C# Web Basics/C# Web Basics - Labs/Lab-06-MVC-Custom View Engine/SUS.HTTP/ResponseCookie.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace SUS.HTTP
+{
+    public class ResponseCookie : Cookie
+    {
+        public ResponseCookie(string name, string value)
+            : base(name, value)
+        {
+            Path = "/";
+        }
+
+
+        public string Path { get; set; }
+
+        public int MaxAge { get; set; }
+
+        public bool HttpOnly { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"{Name}={Value}; Path={Path};");
+
+            if (MaxAge != 0)
+            {
+                sb.Append($" Max-Age={MaxAge}; ");
+            }
+
+            if (HttpOnly)
+            {
+                sb.Append($" HttpOnly;");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
