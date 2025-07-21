24b4ea4bc8165649b83a87ade4612356a12c398a
viliyani <viliyan7@gmail.com>
2025-07-21 23:42:22 +0300

    [JS Adv] (Lab 4 - DOM Intro) Tasks 3,4.

---
+++ JS Advanced/JS Advanced - Labs/Lab-04-DOM/Task04.js
@@ -0,0 +1,7 @@
+function show() {
+    let item = document.getElementById('text');
+    let more = document.getElementById('more');
+
+    more.style.display = 'none';
+    item.style.display = 'inline';
+}
