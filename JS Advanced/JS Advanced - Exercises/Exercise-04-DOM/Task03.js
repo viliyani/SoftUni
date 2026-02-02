fe1d7e568f2610425363187b9598923b4227d42d
viliyani <viliyan7@gmail.com>
2026-02-02 23:23:30 +0200

    [JS Adv] (Ex 4 - DOM Intro) Tasks 3,4.

---
+++ JS Advanced/JS Advanced - Exercises/Exercise-04-DOM/Task03.js
@@ -0,0 +1,11 @@
+function toggle() {
+    const span = document.getElementsByClassName('button')[0];
+    
+    if (span.textContent === 'More') {
+        span.textContent = 'Less';
+        document.getElementById('extra').style.display = 'block';
+    } else {
+        span.textContent = 'More';
+        document.getElementById('extra').style.display = 'none';
+    }
+}
