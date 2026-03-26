33e20cec84752ce18ddc909494c1d65396b5b1cd
viliyani <viliyan7@gmail.com>
2026-03-26 18:45:07 +0200

    [JS Adv] (Ex 4 - DOM Intro) Tasks 5,6.

---
+++ JS Advanced/JS Advanced - Exercises/Exercise-04-DOM/Task06.js
@@ -0,0 +1,21 @@
+function solve() {
+    const items = Array.from(document.getElementsByTagName('li'));
+    const searchedText = document.getElementById('searchText').value;
+    let count = 0;
+    
+    const matches = items.map(
+        el => {
+            el.style.fontWeight = 'normal';
+            el.style.textDecoration = '';
+            if (el.textContent.includes(searchedText) && searchedText) {
+                count++;
+                el.style.fontWeight = 'bold';
+                el.style.textDecoration = 'underline';
+            }
+            
+            return el;
+        }
+    );
+    
+    document.getElementById('result').textContent = `${count} matches found`;
+}
